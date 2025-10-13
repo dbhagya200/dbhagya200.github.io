07de75287503064d79be174da01c9d710795a886
dbhagya200 <dilinibhagya53@gmail.com>
2025-10-13 15:12:22 +0530

    img

--- src/components/section/PortfolioSection.tsx
+++ src/components/section/PortfolioSection.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { FaPlus, FaLink } from 'react-icons/fa';
-import cocothumb from '../../../public/portfolio/cocothumb.png';
-import guppyzone from '../../../public/portfolio/Guppy-Zone.png';
-import echocart from '../../../public/portfolio/EchoCart.png';
-import medicare from '../../../public/portfolio/MediCare.png';
-import notes from '../../../public/portfolio/notes.png';
-import logistics from '../../../public/portfolio/logistics.png';
+import cocothumb from '../../../public/img/cocothumb.png';
+import guppyzone from '../../../public/img/Guppy-Zone.png';
+import echocart from '../../../public/img/EchoCart.png';
+import medicare from '../../../public/img/MediCare.png';
+import notes from '../../../public/img/notes.png';
+import logistics from '../../../public/img/logistics.png';
 
 const PortfolioSection: React.FC = () => {
   const [activeFilter, setActiveFilter] = useState('*');
 
   const portfolioItems = [
-    { id: 1, category: 'web', title: 'Cocothumb Project', image: '../../../public/portfolio/cocothumb.png',Link:'https://github.com/dbhagya200/cocothumb' },
+    { id: 1, category: 'web', title: 'Cocothumb Project', image: `(${cocothumb})`,Link:'https://github.com/dbhagya200/cocothumb' },
     { id: 2, category: 'web', title: 'Guppy-Zone Spring-Boot Project', image: `(${guppyzone})`,Link:'https://github.com/dbhagya200/Guppy-Zone-Project-New' },
     { id: 3, category: 'web', title: 'EchoCart JavaEE Project', image: `(${echocart})`,Link:'https://github.com/dbhagya200/AAD-Assignment-1' },
     { id: 4, category: 'web', title: 'MediCare HTML/PHP Project', image: `(${medicare})`,Link:'https://github.com/Pathum456/bhagya_project' },
