c79ff23250ac1fc1fbf49648100df6bf7dce3b33
dbhagya200 <dilinibhagya53@gmail.com>
2025-10-13 15:26:38 +0530

    img

--- src/components/section/PortfolioSection.tsx
+++ src/components/section/PortfolioSection.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FaPlus, FaLink } from 'react-icons/fa';
 // import cocothumb from '../../../public/img/cocothumb.png';
-import guppyzone from '../../../public/img/Guppy-Zone.png';
+import guppyzone from '../../assets/img/Guppy-Zone.png';
 import echocart from '../../../public/img/EchoCart.png';
 import medicare from '../../../public/img/MediCare.png';
 import notes from '../../../public/img/notes.png';
@@ -11,8 +11,8 @@ const PortfolioSection: React.FC = () => {
   const [activeFilter, setActiveFilter] = useState('*');
 
   const portfolioItems = [
-    { id: 1, category: 'web', title: 'Cocothumb Project', image:'public/img/cocothumb.png',Link:'https://github.com/dbhagya200/cocothumb' },
-    { id: 2, category: 'web', title: 'Guppy-Zone Spring-Boot Project', image: `(${guppyzone})`,Link:'https://github.com/dbhagya200/Guppy-Zone-Project-New' },
+    { id: 1, category: 'web', title: 'Cocothumb Project', image:'/img/cocothumb.png',Link:'https://github.com/dbhagya200/cocothumb' },
+    { id: 2, category: 'web', title: 'Guppy-Zone Spring-Boot Project', image: guppyzone,Link:'https://github.com/dbhagya200/Guppy-Zone-Project-New' },
     { id: 3, category: 'web', title: 'EchoCart JavaEE Project', image: `(${echocart})`,Link:'https://github.com/dbhagya200/AAD-Assignment-1' },
     { id: 4, category: 'web', title: 'MediCare HTML/PHP Project', image: `(${medicare})`,Link:'https://github.com/Pathum456/bhagya_project' },
     { id: 5, category: 'app', title: 'Notes-App React-Native Project ', image: `(${notes})`,Link:'https://github.com/dbhagya200/MY-Notes-MobileApp' },
